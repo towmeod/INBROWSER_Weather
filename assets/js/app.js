7e2891fffd54a4c71a3391e362d1b764d08043f7
Andrew <77741677+towmeod@users.noreply.github.com>
2021-02-18 13:35:27 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
     }
     else {
         // Default city
-        city = "London";
+        city = "";
     }
     // Get and display current date
     date = moment();
